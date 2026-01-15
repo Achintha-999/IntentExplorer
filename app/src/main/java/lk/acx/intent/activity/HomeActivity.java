21ac0b198c699b69268d10aea1986578c82416e5
Achintha-999 <achinthasandaruwan517@gmail.com>
2026-01-15 18:33:04 +0530

    #

--- app/src/main/java/lk/acx/intent/activity/HomeActivity.java
+++ app/src/main/java/lk/acx/intent/activity/HomeActivity.java
@@ -3,15 +3,24 @@ package lk.acx.intent.activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import lk.acx.intent.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected  void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+        Log.i(HomeActivity.class.getSimpleName(), "onNewIntent:");
+        setIntent(intent);
+    }
 
     private Button homeBtn, browerBtn, dialBtn, shareBtn;
 
